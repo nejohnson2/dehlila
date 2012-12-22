5c68cdc55c9ee7e40205592a11c578d4bbcb987c
nejohnson2 <nejohnson2@gmail.com>
2012-12-21 23:39:29 -0500

    added server config

--- web.js
+++ web.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var ejs = require('ejs');
 require('./elizabot.js');
 require('./elizadata.js');
 
@@ -8,6 +9,40 @@ Twilio.AccountSid = "ACad716cc4da934be6ad19bf5353312248";
 Twilio.AuthToken  = "3af91684fa2d040f587bf96955cffd82";
 
 var app = express.createServer(express.logger());
+
+/*********** SERVER CONFIGURATION *****************/
+app.configure(function() {
+    
+    /*********************************************************************************
+        Configure the template engine
+        We will use EJS (Embedded JavaScript) https://github.com/visionmedia/ejs
+        
+        Using templates keeps your logic and code separate from your HTML.
+        We will render the html templates as needed by passing in the necessary data.
+    *********************************************************************************/
+    app.set('port', process.env.PORT || 3000);
+    app.set('view engine','ejs');  // use the EJS node module
+    app.set('views',__dirname+ '/views'); // use /views as template directory
+    app.set('view options',{layout:true}); // use /views/layout.html to manage your main header/footer wrapping template
+    app.register('html',require('ejs')); //use .html files in /views
+
+    /******************************************************************
+        The /static folder will hold all css, js and image assets.
+        These files are static meaning they will not be used by
+        NodeJS directly. 
+        
+        In your html template you will reference these assets
+        as yourdomain.heroku.com/img/cats.gif or yourdomain.heroku.com/js/script.js
+    ******************************************************************/
+    app.use(express.static(__dirname + '/static'));
+    //parse any http form post
+    app.use(express.bodyParser());
+    
+    /**** Turn on some debugging tools ****/
+    app.use(express.logger());
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+
+});
 
 app.get('/', function(request, response) {
 /*
